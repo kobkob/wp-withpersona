ce09a28b80b542c65707af97d04d33e590b3efb1
Thiago Vieira <thiago.a.vieira@hotmail.com>
2025-08-07 01:49:23 +0000

    Bump version to 1.2.10

--- wp-withpersona.php
+++ wp-withpersona.php
@@ -8,7 +8,7 @@
  * Author URI:      https://kobkob.org/
  * Text Domain:     wp-withpersona
  * Domain Path:     /languages
- * Version: 1.2.9
+ * Version: 1.2.10
  *
  * @package         wp-withpersona
  */
